8c4dae56d76c6fcc10f64f0e07b8960788ed495c
jinseongjoo <qpyou1234@naver.com>
2018-08-06 21:56:02 +0900

    Setting up React Router

--- movieql-client/src/App.js
+++ movieql-client/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo';
+import { HashRouter as Router, Route } from 'react-router-dom'
 import client from './apolloClient'
+import Home from './HOME'
+import Detail from './Detail'
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="App" />
+        <Router>
+          <React.Fragment>
+            <Route exact={true} path={"/"} component={Home} />
+            <Route path={"/details/:movieId"} component={Detail} />
+          </React.Fragment>
+        </Router>  
       </ApolloProvider>
     )
   }
