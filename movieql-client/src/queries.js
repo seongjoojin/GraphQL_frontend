2ea8975606a98eea1b093ae81115bcdb0121f647
jinseongjoo <qpyou1234@naver.com>
2018-08-07 22:47:21 +0900

    Creating a Query with variables

--- movieql-client/src/queries.js
+++ movieql-client/src/queries.js
@@ -11,4 +11,19 @@ export const HOME_PAGE = gql`
   }
 `;
 
-export const MOVIE_DETAILS = gql``
+export const MOVIE_DETAILS = gql`
+query getMovieDetails($movieId:Int!){
+  movie(id : $movieId){
+    medium_cover_image
+    title
+    rating
+    description_intro
+    language
+    genres
+  }
+  suggestions(id:$movieId){
+    medium_cover_image
+    title
+    rating
+  }
+}`;
